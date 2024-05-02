4a2ec7dc9713c60c2c999b4a028bc193b0e86aac
abdiwoli <abdiwolih@gmail.com>
2024-05-02 08:34:38 +0300

    doneTask

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -4,12 +4,15 @@
 
 def canUnlockAll(boxes):
     """ ulock boxes """
+
+    if (type(boxes)) is not list:
+        return False
     idx = 0
     ln = len(boxes)
     opened = set()
     opened.add(0)
-    if ln <= 0:
-        return True
+    if ln == 0:
+        return False
     for box in boxes:
         if (not box):
             break
