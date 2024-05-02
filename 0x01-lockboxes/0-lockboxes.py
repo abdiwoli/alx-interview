f9c210b8b0d23f7119ca756f21b5a05b8fd2b6e8
abdiwoli <abdiwolih@gmail.com>
2024-05-02 16:18:19 +0300

    test

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -7,21 +7,20 @@ def canUnlockAll(boxes):
 
     if (type(boxes)) is not list:
         return False
-    ln = len(boxes)
+    arr = []
+    arr.append(0)
     opened = []
-    opened.append(boxes[0])
-    if ln == 0:
-        return False
     for box in boxes:
-        if (not box):
-            opened.append(box)
+        for el in box:
+            if el not in arr:
+                arr.append(el)
+        if not box:
             break
-        for i in box:
-            if i < ln and i > 0:
-                if box not in opened:
-                    opened.append(box)
-                else:
-                    break
-    for box in opened:
-        boxes.remove(box)
+    for idx in arr:
+        try:
+            opened.append(boxes[idx])
+        except:
+            pass
+    for i in opened:
+        boxes.remove(i)
     return len(boxes) == 0
