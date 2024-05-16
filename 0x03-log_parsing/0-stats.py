157e822fce1bb5589842ecce20fb1646fdc33544
abdiwoli <abdiwolih@gmail.com>
2024-05-16 23:48:59 +0300

    fix

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -25,15 +25,24 @@ def is_valid(input_string):
 if __name__ == "__main__":
     status = dict()
     total_size = []
+    line_count = 0
     for line in sys.stdin:
         key, size = is_valid(line)
         if key and size:
             val = status.get(key, 0)
             status[key] = val + 1
             total_size.append(size)
-        if len(total_size) == 10:
+            line_count += 1
+        if line_count == 10:
             print(f"File size: {sum(total_size)}")
             for i in status.keys():
                 print(f"{i}: {status[i]}")
             status.clear()
             total_size.clear()
+            line_count = 0
+
+    # Check if there are any remaining lines to process
+    if line_count > 0:
+        print(f"File size: {sum(total_size)}")
+        for i in status.keys():
+            print(f"{i}: {status[i]}")
