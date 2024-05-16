72ac7a5f2253f58faf88ab13f7e6146f28c5acd7
abdiwoli <abdiwolih@gmail.com>
2024-05-17 01:30:11 +0300

    done

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,32 +1,30 @@
 #!/usr/bin/python3
-""" std parsing """
-import re
+""" import module """
 import sys
+import re
+from collections import defaultdict
 
 
-def is_valid(input_string):
-    """ is valid input """
-    pattern = (
-        r'^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - \[(.*?)\] '
-        r'"GET /projects/260 HTTP/1\.1" (\d{3}) (\d+)$')
-    regex = re.compile(pattern)
-    match = regex.match(input_string)
+def is_valid_input(input_string):
+    """Checks if a line matches the expected access log format a.
+    """
+    pattern = r"^(\S+) - \[.*?\] \"GET /projects/260 HTTP/1.1\" (\d{3}) (\d+)$"
+    match = re.match(pattern, input_string.strip())
     if match:
         try:
-            status_code = int(match.group(3))
-            file_size = int(match.group(4))
-        except TypeError as e:
-            return None, None
-        return status_code, file_size
-    else:
-        return None, None
+            status_code = int(match.group(2))
+            file_size = int(match.group(3))
+            return True, status_code, file_size
+        except ValueError:
+            pass
+    return False, None, None
 
 
 def print_out(total_size, status):
-    """ print the output """
-    print("File size: {:d}".format(sum(total_size)))
-    for k, v in sorted(status.items()):
-        print(f"{k}: {v}")
+    """Prints the calculated statistics"""
+    print(f"File size: {sum(total_size)}")
+    for code, count in sorted(status.items()):
+        print(f"{code}: {count}")
 
 
 if __name__ == "__main__":
@@ -35,16 +33,13 @@ if __name__ == "__main__":
     line_count = 0
     try:
         for line in sys.stdin:
-            key, size = is_valid(line.strip())
             line_count += 1
-            if key and size:
-                val = status.get(key, 0)
-                status[key] = val + 1
-                total_size.append(size)
-            if line_count == 10:
+            is_valid, status_code, file_size = is_valid_input(line)
+            if is_valid:
+                total_size.append(file_size)
+                status[status_code] = status.get(status_code, 0) + 1
+            if line_count % 10 == 0:
                 print_out(total_size, status)
-                line_count = 0
-
     except KeyboardInterrupt:
         print_out(total_size, status)
         raise
