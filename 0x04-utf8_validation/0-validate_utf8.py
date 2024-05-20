e5d012b2fbe63d4bbe75fd3b5a83ad41df62b763
abdiwoli <abdiwolih@gmail.com>
2024-05-20 18:17:46 +0300

    done

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" main function """
+
+
+def validUTF8(data):
+    """ determine utf8 """
+    cont_bytes = 0
+
+    for byte in data:
+        if cont_bytes == 0:
+            if byte >> 5 == 0b110:
+                cont_bytes = 1
+            elif byte >> 4 == 0b1110:
+                cont_bytes = 2
+            elif byte >> 3 == 0b11110:
+                cont_bytes = 3
+            elif byte >> 7 != 0:
+                return False
+        else:
+            if byte >> 6 != 0b10:
+                return False
+            cont_bytes -= 1
+
+    return cont_bytes == 0
