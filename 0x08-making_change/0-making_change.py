9618bb8e0cf5d25692ba18bae5f7e6f432e3adbf
abdiwoli <abdiwolih@gmail.com>
2024-06-21 00:08:41 +0300

    done

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,17 @@
+##!/usr/bin/python3
+""" alx interview """
+
+
+def makeChange(coins, amount):
+    """ make change """
+    if amount is None or amount <= 0:
+        return 0
+    dp = [float('inf')] * (amount + 1)
+    dp[0] = 0
+
+    for i in range(1, amount + 1):
+        for coin in coins:
+            if i - coin >= 0:
+                dp[i] = min(dp[i], dp[i - coin] + 1)
+
+    return dp[amount] if dp[amount] != float('inf') else -1
